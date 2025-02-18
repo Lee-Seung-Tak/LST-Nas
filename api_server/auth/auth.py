612d81d2c52d008f7ef0506740529628ec4f095c
lstlove9804 <lstlove9804@ednanet.com>
2025-02-19 01:01:23 +0900

    auth 모듈 token 인증 누락된 것 추가

--- api_server/auth/auth.py
+++ api_server/auth/auth.py
@@ -1,10 +1,31 @@
-from typing import Union
-from fastapi import FastAPI, Response, APIRouter, Form, status
+from typing import Optional
+from fastapi import FastAPI, Response, APIRouter, status, Form, Depends
 from config import supabase
+from config import oauth2_scheme
 
 app    = FastAPI()
 router = APIRouter(prefix='/auth')
 
+async def check_user_role( token: str, option: Optional[bool] = None ):
+    
+    response        = supabase.auth.get_user( token )
+    user_id         = response.user.id
+    check_user_role = supabase.table('roles').select('*').eq('id',user_id).eq('role','admin').execute()
+    
+    if check_user_role.data != []:
+        
+        if option is not None :
+            return True, user_id
+        else :
+            return True
+        
+    else:
+        
+        if option is not None :
+            return False, None
+        else :
+            return False
+            
 
 @router.post('/login')
 async def login( email:str = Form(...), pw:str = Form(...) ):
@@ -26,7 +47,18 @@ async def login( email:str = Form(...), pw:str = Form(...) ):
 
 
 @router.post('/refresh')
-async def token_refresh( access_token:str = Form(...), refresh_token:str = Form(...) ):
+async def token_refresh( access_token:str = Form(...), refresh_token:str = Form(...), token: str= Depends( oauth2_scheme )  ):
+    
+    if token is None:
+        return Response('access_token required.', status_code=status.HTTP_401_UNAUTHORIZED)
+    
+    try :
+        usser_status = await check_user_role( token )
+        if usser_status == False :
+            return Response('Invalid Token', status_code=status.HTTP_401_UNAUTHORIZED)
+    except Exception as e:
+        return Response('Invalid Token', status_code=status.HTTP_401_UNAUTHORIZED)
+    
     if access_token is None or refresh_token is None :
         return Response('Token required.', status_code=status.HTTP_400_BAD_REQUEST)
     
@@ -45,7 +77,18 @@ async def token_refresh( access_token:str = Form(...), refresh_token:str = Form(
 
 
 @router.post('/logout')
-async def logout( access_token:str = Form(...), refresh_token:str = Form(...) ):
+async def logout( access_token:str = Form(...), refresh_token:str = Form(...) , token: str= Depends( oauth2_scheme ) ):
+    
+    if token is None:
+        return Response('access_token required.', status_code=status.HTTP_401_UNAUTHORIZED)
+    
+    try :
+        usser_status = await check_user_role( token )
+        if usser_status == False :
+            return Response('Invalid Token', status_code=status.HTTP_401_UNAUTHORIZED)
+    except Exception as e:
+        return Response('Invalid Token', status_code=status.HTTP_401_UNAUTHORIZED)
+    
     if access_token is None or refresh_token is None :
         return Response('Token required.', status_code=status.HTTP_400_BAD_REQUEST)
     
@@ -55,5 +98,6 @@ async def logout( access_token:str = Form(...), refresh_token:str = Form(...) ):
         
         return Response("Logout Success.", status_code=status.HTTP_200_OK)
     
-    except:
+    except Exception as e:
+        print(e)
         return Response('Check token information.', status_code=status.HTTP_401_UNAUTHORIZED)
